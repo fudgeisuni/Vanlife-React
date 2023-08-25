aa5f116164d99027a08a3d3f9636a0b30cf30d90
fudgeisuni <96190307+fudgeisuni@users.noreply.github.com>
2023-08-25 14:39:09 +0100

    1 file modified

--- src/VanDetail.js
+++ src/VanDetail.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useParams } from "react-router-dom"
+import "./style.css";
 
 export default function VanDetail(){
     const params = useParams()
@@ -11,19 +12,22 @@ export default function VanDetail(){
       fetch("/api/vans/" + params.id.toString())
           .then(res => res.json())
           .then(data => setVan(data.vans))
-    }, [])
+    }, [params.id])
 
 
     
 
     return(
+     <div>
+     {van ? (   
      <div key={van.id} className="van-tile">
         <img class="van-image" src={van.imageUrl} />
         <div className="van-info">
           <h3>{van.name}</h3>
           <p>${van.price}<span>/day</span></p>  
         </div>
-        
+      </div>
+      ) : <h2>Loading</h2>}
       </div>
     )
 }
