554a8902d18c48cd6c784729b14cb936569150a7
fudgeisuni <96190307+fudgeisuni@users.noreply.github.com>
2023-08-30 21:26:01 +0100

    1 file modified

--- src/HostLayout.js
+++ src/HostLayout.js
@@ -1,21 +1,33 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link, NavLink} from "react-router-dom"
 import {Outlet} from "react-router-dom"
 
 export default function HostLayout(){
+  
+  const activeStyle = {
+    fontWeight: "600",
+    textDecoration: "underline",
+    color: "yellow"
+  }
+
+
   return(
     <>
     <nav>
     <table>
       <tr>
       <td class="navLink">
-      <Link class="splashText" to="/host">Dashboard</Link>
+      <NavLink class="splashText" to="/host"
+      end
+      style={({isActive}) => isActive ? activeStyle : null}>Dashboard</NavLink>
       </td>
       <td class="navLink">
-      <Link class="splashText" to = "/host/income">Income</Link>
+      <NavLink class="splashText" to = "/host/income"
+      style={({isActive}) => isActive ? activeStyle : null}>Income</NavLink>
       </td>
       <td class="navLink">
-      <Link class="splashText" to = "/host/reviews">Reviews</Link>
+      <NavLink class="splashText" to = "/host/reviews" 
+      style={({isActive}) => isActive ? activeStyle : null}>Reviews</NavLink>
       </td>
       </tr>
     </table>
