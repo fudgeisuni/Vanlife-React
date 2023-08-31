4327967d2de7d68e9fb466ea98efdd32cc8780b2
fudgeisuni <96190307+fudgeisuni@users.noreply.github.com>
2023-08-31 15:58:35 +0100

    1 file modified

--- src/HostVans.js
+++ src/HostVans.js
@@ -1,6 +1,29 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 
 export default function HostVans(){
+
+  const [vans, setVans] = React.useState([]);
+
+  React.useEffect(function() {
+    fetch("/api/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+  }, [])
+  
+  const vanElements = vans.map(van => (
+    <div key={van.id} className="van-tile">
+      <Link  class="navText" to={`/vans/${van.id}`}>
+      <img class="van-image" src={van.imageUrl} />
+      </Link>
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>${van.price}<span>/day</span></p>  
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ))
+
   return(
     <div class="hostVans">
          <h1>Your listed vans</h1> 
