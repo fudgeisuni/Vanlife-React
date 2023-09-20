7995bbe81c82ae0672379774971ae0596d452f34
fudgeisuni <96190307+fudgeisuni@users.noreply.github.com>
2023-09-20 19:45:20 +0100

    1 file modified

--- src/HostVanInfo.js
+++ src/HostVanInfo.js
@@ -3,7 +3,7 @@ import { useParams, Link, useOutletContext } from "react-router-dom"
 import "./style.css";
 
 export default function HostVanInfo(){
-    const [van, setVan] = useOutletContext();
+    const [van] = useOutletContext();
     return(
       <div class="vanInfoDiv">
         <p><p class="headerPara">Name:</p>{van.name}</p>
